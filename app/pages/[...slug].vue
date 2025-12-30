078f2a974f802cc0e831ff907d63be79bf3812d6
Lucas-Aslan <Lucas06.04@hotmail.com>
2025-12-30 14:24:52 +0100

    Consolidate styles into global stylesheet

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -19,23 +19,3 @@ if (!page.value) {
     :value="page"
   />
 </template>
-
-<style scoped>
-.datenschutz-content :deep(h1),
-.datenschutz-content :deep(h2),
-.datenschutz-content :deep(h3),
-.datenschutz-content :deep(h4),
-.datenschutz-content :deep(h5),
-.datenschutz-content :deep(h6) {
-  color: #ffffff !important;
-}
-
-.datenschutz-content :deep(h1 a),
-.datenschutz-content :deep(h2 a),
-.datenschutz-content :deep(h3 a),
-.datenschutz-content :deep(h4 a),
-.datenschutz-content :deep(h5 a),
-.datenschutz-content :deep(h6 a) {
-  color: inherit;
-}
-</style>
